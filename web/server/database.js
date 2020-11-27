a541e493563dc292d052063e4e061e7a6f468dc7
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-27 08:19:03 +0000

    add property feature implemented

--- web/server/database.js
+++ web/server/database.js
@@ -116,7 +116,7 @@ const getAllProperties = function(options, limit = 10) {
   // 3
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    queryString += `WHERE properties.owner_id = $${queryParams.length} `;
+    queryString += `AND properties.owner_id = $${queryParams.length} `;
   }
   if (options.city) {
     queryParams.push(`%${options.city}%`);
@@ -158,9 +158,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  for (const items of Object.keys(property)) {
+    queryParams.push(property[items]);
+  }
+
+
+  return pool.query(`
+    INSERT INTO properties (title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,street,city,province,post_code,country,parking_spaces,number_of_bathrooms,number_of_bedrooms,owner_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *
+  `,queryParams)
+    .then((res) => {
+      console.log(res.rows);
+      return res.rows[0];
+    });
 };
 exports.addProperty = addProperty;
